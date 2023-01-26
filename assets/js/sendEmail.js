465abe61ca178ce9027eb4b87a55dd56ba9aeddb
Nic <nicolecadman@icloud.com>
2023-01-26 11:07:12 +0000

    made all fields required

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,27 +1,23 @@
 function SendMail() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("emailaddress").value;
+    var message = document.getElementById("message").value;
+    
+    if (!name || !email || !message) {
+        alert("All fields are required.");
+        return;
+    }
     var params = {
-        from_name : document.getElementById("name").value,
-        from_email : document.getElementById("emailaddress").value,
-        message: document.getElementById("message").value
+        from_name : name,
+        from_email : email,
+        message: message
     }
     emailjs.send("service_af0lief", "template_52av2hc", params).then(function (res) {
+        var message = document.createElement("div");
+        var form = document.getElementById("contact-form");
+        message.innerHTML = "Your message has been successfully sent!";
+        message.classList.add("success-message");
+        form.appendChild(message);
         document.getElementById("contact-form").reset();
     })
 }
-
-// Get the submit button
-var submitButton = document.getElementById("submit-button");
-// Get the form element
-var form = document.getElementById("contact-form");
-
-// Add a click event listener to the button
-submitButton.addEventListener("click", function() {
-    // Add the success message
-    var message = document.createElement("div");
-    message.innerHTML = "Your message has been successfully sent!";
-    message.classList.add("success-message");
-    form.appendChild(message);
-
-    // Clear the form fields
-    form.reset();
-});
