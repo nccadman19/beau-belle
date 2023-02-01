75cae652c2193c02aef93b3d70331386b4f5cfc5
Nic <nicolecadman@icloud.com>
2023-02-01 22:05:08 +0000

    Started created quiz, js started and css designed

---
+++ assets/js/quiz.js
@@ -0,0 +1,8 @@
+const question = document.querySelector('#question');
+const choices = document.querySelectorAll('.choices-text');
+const answer = document.querySelector('#answer');
+
+let currentQuestion = {};
+let answerSelected = true;
+let availableQuestions = []; 
+
