c804885c82f7a12a27864db5900356aabc1cd2ac
Nic <nicolecadman@icloud.com>
2023-02-03 17:29:20 +0000

    Made the questions appear on screen, started css

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,53 +1,133 @@
-let question1 = "Do you like extending your nails or natural looking nails?";
-let option1 = "fake tips";
-let option2 = "natural looking nails";
+window.onload=function() {
+    let question1 = document.getElementById("question1");
+    let fakeTipsButton = document.getElementById("fake-tips");
+    let naturalNailsButton = document.getElementById("natural-nails");
+    let startButton = document.getElementById("start-button");
+    let quiz = document.getElementById("quiz");
+    let quizPage = document.getElementById("quiz-page");
+    
+    question1.style.display = "none";
+    startButton.addEventListener("click", function() {
+        question1.style.display = "block";
+        startButton.style.display = "none";
+        quizPage.appendChild(question1);
+    });
+    quiz.appendChild(quizPage);
 
-let response = prompt(question1 + '\n' + option1 + '\n' + option2);
+    fakeTipsButton.addEventListener("click", function() {    
+        question1.style.display = "none";
+        startButton.style.display = "none";
 
-if (response === option1) {
-  let question2 = 'Do you like a fancy design or a simple one?';
-  let option1 = 'fancy';
-  let option2 = 'simple';
-  
-  response = prompt(question2 + '\n' + option1 + '\n' + option2);
+        let question2 = document.createElement("div");
+        question2.innerHTML = "Do you like a fancy design or a simple one?" +
+                                "<br><br>" +
+                                "<button id='fancy-nails'>Fancy</button>" +
+                                "<button id='simple-nails'>Simple</button>";
+        quizPage.appendChild(question2);
 
-  if (response === option1) {
-    console.log("You should book Design X Pro Extensions Full Nail Art");
-  } else {
-    console.log("You should book Design X Pro Extensions Basic Nail Art.");
-  }
-} else {
-  let question2 = "Are you happy with your nail length or are you trying to grow them?";
-  let option1 = 'happy';
-  let option2 = 'grow';
-  
-  response = prompt(question2 + '\n' + option1 + '\n' + option2);
+        let fancyNailsButton = document.getElementById("fancy-nails");
+        let simpleNailsButton = document.getElementById("simple-nails");
 
-if (response === option1) {
-  let question3 = 'Do you like a fancy design or a simple one?';
-  let option1 = 'fancy';
-  let option2 = 'simple';
-  
-  response = prompt(question3 + '\n' + option1 + '\n' + option2);
+        fancyNailsButton.addEventListener("click", function() {
+            var answer1 = document.createElement("div");
+            var quiz = document.getElementById("quiz");
+            question2.style.display = "none";
+            answer1.innerHTML = "You should book Design X Pro Extensions Full Nail Art";
+            answer1.classList.add("success-message");
+            quizPage.appendChild(answer1);
+        });
 
-  if (response === option1) {
-    console.log("You should book Gel Polish Manicure Full Nail Art.");
-  } else {
-    console.log("You should book Gel Polish Manicure Basic Nail Art");
-  }
-  }
+        simpleNailsButton.addEventListener("click", function() {
+            var answer1 = document.createElement("div");
+            var quiz = document.getElementById("quiz");
+            question2.style.display = "none";
+            answer1.innerHTML = "You should book Design X Pro Extensions Basic Nail Art";
+            answer1.classList.add("success-message");
+            quizPage.appendChild(answer1);
+        });
+    });
 
-if (response === option2) {
-  let question4 = 'Do you like a fancy design or a simple one?';
-  let option1 = 'fancy';
-  let option2 = 'simple';
-  
-  response = prompt(question4+ '\n' + option1 + '\n' + option2);
+        naturalNailsButton.addEventListener("click", function() {
+        question1.style.display = "none";
+        startButton.style.display = "none";
 
-  if (response === option2) {
-    console.log("You should book BIAB Full Nail Art.");
-  } else {
-    console.log("You should book BIAB Basic Nail Art");
-  }
-  }
+        let question2 = document.createElement("div");
+        question2.innerHTML = "Are you happy with your nail length or are you trying to grow them?" +
+                                "<br><br>" +
+                                "<button id='happy-nails'>Happy</button>" +
+                                "<button id='grow-nails'>Grow</button>";
+        quizPage.appendChild(question2);
+
+        let happyNailsButton = document.getElementById("happy-nails");
+        let growNailsButton = document.getElementById("grow-nails");
+
+        happyNailsButton.addEventListener("click", function() {
+            question1.style.display = "none";
+            question2.style.display = "none";
+            startButton.style.display = "none";
+
+            let question3 = document.createElement("div");
+            question3.innerHTML = "Do you like a fancy design or a simple one?" +
+                                "<br><br>" +
+                                "<button id='fancy-nails'>Fancy</button>" +
+                                "<button id='simple-nails'>Simple</button>";
+            quizPage.appendChild(question3);
+        
+            let fancyNailsButton = document.getElementById("fancy-nails");
+            let simpleNailsButton = document.getElementById("simple-nails");
+        
+            fancyNailsButton.addEventListener("click", function() {
+                var answer1 = document.createElement("div");
+                var quiz = document.getElementById("quiz");
+                question3.style.display = "none";
+                answer1.innerHTML = "You should book Gel Polish Manicure Full Nail Art";
+                answer1.classList.add("success-message");
+                quizPage.appendChild(answer1);
+            });
+        
+            simpleNailsButton.addEventListener("click", function() {
+                var answer1 = document.createElement("div");
+                var quiz = document.getElementById("quiz");
+                question3.style.display = "none";
+                answer1.innerHTML = "You should book Gel Polish Manicure Basic Nail Art";
+                answer1.classList.add("success-message");
+                quizPage.appendChild(answer1);
+            });
+        });
+
+            growNailsButton.addEventListener("click", function() {
+                question1.style.display = "none";
+                question2.style.display = "none";
+                startButton.style.display = "none";
+
+                let question3 = document.createElement("div");
+                question3.innerHTML = "Do you like a fancy design or a simple one?" +
+                                    "<br><br>" +
+                                    "<button id='fancy-nails'>Fancy</button>" +
+                                    "<button id='simple-nails'>Simple</button>";
+                quizPage.appendChild(question3);
+            
+                let fancyNailsButton = document.getElementById("fancy-nails");
+                let simpleNailsButton = document.getElementById("simple-nails");
+            
+                fancyNailsButton.addEventListener("click", function() {
+                    var answer1 = document.createElement("div");
+                    var quiz = document.getElementById("quiz");
+                    question3.style.display = "none";
+                    answer1.innerHTML = "You should book BIAB Manicure Full Nail Art";
+                    answer1.classList.add("success-message");
+                    quizPage.appendChild(answer1);
+                });
+            
+                simpleNailsButton.addEventListener("click", function() {
+                    var answer1 = document.createElement("div");
+                    var quiz = document.getElementById("quiz");
+                    question3.style.display = "none";
+                    answer1.innerHTML = "You should book BIAB Manicure Basic Nail Art";
+                    answer1.classList.add("success-message");
+                    quizPage.appendChild(answer1);
+                });
+            
+            });
+    });
 }
